2ac9deb4175485dd1061dcfad624afc2bdc3d438
adityaoryza <oryza429@gmail.com>
2023-04-24 00:57:41 +0700

    add some code

--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 var express = require("express");
 var app = express();
 var port = process.env.PORT || 3000;
-
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
@@ -21,10 +20,22 @@ app.get("/", function (req, res) {
 
 // your first API endpoint...
 app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+  // console.log({ greeting: "hello API" });
+  // res.json({ greeting: "hello API" });
+  res.write("<h1>hello API</h1>");
+  res.send("pesan berhasil dikirim");
+});
+
+app.post("/api/hello", function (req, res) {
+  res.send("pesan berhasil dikirim");
+});
+
+app.get("/api/timestamp/:date_string", function (req, res) {
+  console.log(req);
+  res.json({ error: "invalid date" });
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(port, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
